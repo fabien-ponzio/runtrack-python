f257cda1501c8b1a90eb965a737746f78bf2d465
Fabien Ponzio <fabien.ponzio@laplateforme.io>
2022-04-19 17:21:24 +0200

    Job 19 not done

---
+++ job13/main.py
@@ -0,0 +1,19 @@
+tab = []
+
+a = input("entrez un nombre entier:") 
+tab.append(a)
+
+b = input("entrez un nombre entier:") 
+tab.append(b)
+
+c = input("entrez un nombre entier:") 
+tab.append(c)
+
+d = input("entrez un nombre entier:") 
+tab.append(d)
+
+e = input("entrez un nombre entier:") 
+tab.append(e)
+ 
+tab.sort()
+print("tri du tableau", tab)
